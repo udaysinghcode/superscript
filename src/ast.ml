a71b3fd683c62a2e67b1883a03a15cb92afc6c56
Michelle Zheng <myz2103@columbia.edu>
2015-10-21 19:24:13 -0400

    Adding comments to AST

--- src/ast.ml
+++ src/ast.ml
@@ -1,15 +1,15 @@
 type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq
 
-type expr =
-  Int of int
-  | Float of float
-  | Boolean of int
-  | String of string
-  | Nil
-  | Id of string
-  | Assign of string * expr
-  | Binop of expr * op * expr
-  | Script of string * expr list
+type expr =				(* Expressions *)
+  Int of int				(* 4 *)
+  | Float of float			(* 4.444 *)
+  | True				(* true *)
+  | String of string			(* "hello world" *)
+  | Nil					(* (): empty list, false *)
+  | Id of string			(* caml_riders *)
+  | Assign of string * expr		(* x = 5 *)
+  | Binop of expr * op * expr		(* x + 10 *)
+  | Script of string * expr list	(* foo 5 21 *)
 
 type stmt = 				(* Statements *)
    Block of stmt list			(* newline-separated stmts *)
