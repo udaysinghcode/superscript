26bdfac40f34dd476216ce0e0f6433e7de371cfc
Michelle Zheng <myz2103@columbia.edu>
2015-10-21 21:37:05 -0400

    Adds list datatype, nil, and boolean as true/false

--- src/ast.ml
+++ src/ast.ml
@@ -1,13 +1,15 @@
 type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq
+type bool = true | false
 
 type expr =				(* Expressions *)
   Int of int				(* 4 *)
   | Float of float			(* 4.444 *)
-  | True				(* true *)
+  | Boolean of bool	 		(* true, false *)
   | String of string			(* "hello world" *)
-  | Nil					(* (): empty list, false *)
   | Id of string			(* caml_riders *)
   | Assign of string * expr		(* x = 5 *)
   | Binop of expr * op * expr		(* x + 10 *)
   | Script of string * expr list	(* foo 5 21 *)
+  | Nil					(* null datatype *)
+  | List of expr			(* list, mixed datatypes allowed in same list *)
   | Cond of expr * expr * expr		(* (if a) b c, equivalent to (if a then b else c) *)
