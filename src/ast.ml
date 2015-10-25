21f5bf5d5b63f18a841549aacd14caab07270e84
Samurdha Jayasinghe <sj2564@columbia.edu>
2015-10-25 19:21:40 -0400

    add function to stringify ast

--- src/ast.ml
+++ src/ast.ml
@@ -14,3 +14,21 @@ type expr =				(* Expressions *)
   | Func of string * expr list		(* foo 5 21 *)
   | Nil					(* null datatype *)
   | List of expr list			(* list, mixed datatypes allowed in same list *)
+
+let rec stringify e = 
+  let stringify_op o = match o with
+    Add -> "Add" | Sub -> "Sub" | Mult -> "Mult" | Div -> "Div"
+    | Addf -> "Addf" | Subf -> "Subf" | Multf -> "Multf" | Divf -> "Divf"
+    | Equal -> "Equal" | Neq -> "Neq" | Less -> "Less" | Leq -> "Leq"
+    | Greater -> "Greater" | Geq -> "Geq" | And -> "And" | Or -> "Or" in
+  let concat l = (String.concat "" l) in
+  match e with
+  | Int(x) -> concat ["Int("; string_of_int x; ")"]
+  | Float(x) -> concat ["Float("; string_of_float x; ")"]
+  | Boolean(x) -> concat ["Boolean("; if x = true then "true" else "false"; ")"]
+  | String(x) -> concat ["String("; x; ")"]
+  | Id(x) -> concat ["Id("; x; ")"]
+  | Assign(str, exp) -> concat ["Assign("; str; ", "; stringify exp; ""]
+  | Binop(exp1, op, exp2) -> concat ["Binop("; stringify exp1; ", "; stringify_op op; ", "; stringify exp2; ")"]
+  | Nil -> "Nil"
+  | List(expl) -> concat ["List("; (String.concat ", " (List.map (fun x -> stringify x) expl)); ")"]
