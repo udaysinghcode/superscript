4a408fb029180e97ea240e0420be2aba16f6a13b
Michelle Zheng <myz2103@columbia.edu>
2015-11-12 10:33:16 -0500

    Updating comments in AST to show examples of correct Superscript syntax

--- src/ast.ml
+++ src/ast.ml
@@ -9,17 +9,17 @@ type expr =				(* Expressions *)
   | Boolean of bool	 		(* true, false *)
   | String of string			(* "hello world" *)
   | Id of string			(* caml_riders *)
-  | Assign of string * expr		(* x = 5 *)
-  | Binop of expr * op * expr		(* x + 10 *)
-  | Eval of string * expr list		(* foo 5 21 *)
-  | Evalarith of op * expr list   (* + 1 2 3*)
+  | Assign of string * expr		(* {x = 5} OR (= x 5) *)
+  | Binop of expr * op * expr		(* {x + 10} *)
+  | Eval of string * expr list		(* (foo 5 21) *)
+  | Evalarith of op * expr list   	(* (+ 1 2 3 ) *)
   | Nil					(* null datatype *)
   | List of expr list			(* list, mixed datatypes allowed in same list *)
-  | Fdecl of string list * expr 	(* fn (a b) (a + b) *)
-  | If of expr * expr * expr
-  | For of expr * expr * expr * expr
-  | While of expr * expr
-  | Let of string * expr * expr
+  | Fdecl of string list * expr 	(* (fn (a b) {a + b}) *)
+  | If of expr * expr * expr		(* (if a b c) *)
+  | For of expr * expr * expr * expr	(* (for a b c d) *)
+  | While of expr * expr		(* (while a b) *)
+  | Let of string * expr * expr		(* (let a b) *)
 
 type program = expr list
 
