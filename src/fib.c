e33e305a15f74e2bde90e60026c605b882135ccd
Michelle Zheng <myz2103@columbia.edu>
2015-12-18 15:27:51 -0500

    Fixing parser - infix expressions using curly braces instead of () within the expr. And calling with infix args: foo({x + 1})

--- src/fib.c
+++ src/fib.c
@@ -1,16 +1,16 @@
 
-(*= (fib 
-   fn (x)
-     if {x is 0}
-        0
-        if {x is 1}
-           1
-           {fib{x-1} + fib{x-2}}) *)
 = (fib 
    fn (x)
      if {x is 0}
         0
         if {x is 1}
            1
-           +(fib{x-1} fib{x-2}))
+           {fib({x-1}) + fib({x-2})})
+= (fib 
+   fn (x)
+     if {x is 0}
+        0
+        if {x is 1}
+           1
+           +(fib({x-1}) fib({x-2})))
 
