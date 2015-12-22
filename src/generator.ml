261bcd2ffbd17725ff92525e71cc342203d72934
Phoebe Wang <yw2684@columbia.edu>
2015-12-22 05:06:43 -0500

    fix string function call in generator.

--- src/generator.ml
+++ src/generator.ml
@@ -310,9 +310,7 @@ let generate_js_func fname =
       ("'function(i) { \
           __assert_arguments_num(arguments.length, 1, \\'string\\'); \
           if (__assert_type(\\'string\\', i, \\'string\\')) { \
-            throw new TypeError(\\'not a string!\\'); \
-          } else { \
-            return i; \
+            return i;
           } \
         }'", [TSome], TString, ["type"])
 
